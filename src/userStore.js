700d760d53d6c51673f1a89b6e70ce68e0d092cc
jinha_yoo <jinha_yoo@tmax.co.kr>
2023-11-20 16:34:43 +0900

    mail식 변경

--- src/userStore.js
+++ src/userStore.js
@@ -1,8 +1,8 @@
 const { v4: uuidv4 } = require("uuid");
 users = [
   {
-    id: "memoreal",
-    username: "jinha",
+    id: "memoreal@tmax.co.kr",
+    username: "tmax",
     password: "1234",
     secrey_key: uuidv4(),
   },
